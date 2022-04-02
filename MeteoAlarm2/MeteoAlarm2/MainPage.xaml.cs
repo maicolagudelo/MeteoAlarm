c9c755f773a68572a63c534d54d7d8ff8fdaa3dd
maico <meteoalarm01@gmil.com>
2022-04-02 17:37:58 -0500

    Agregar archivos de proyecto.

---
+++ MeteoAlarm2/MeteoAlarm2/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MeteoAlarm2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReg_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabbedPage1());
+        }
+
+        private async void btnRegis_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterMA());
+        }
+    }
+}
---
+++ MeteoAlarm2/MeteoAlarm2/RegisterMA.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MeteoAlarm2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterMA : ContentPage
+    {
+        public RegisterMA()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabbedPage1());
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
